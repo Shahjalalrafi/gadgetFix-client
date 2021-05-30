3f9e0daa98a688be15a36eede4ff8669ee2566e9
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-05-30 21:32:10 +0600

    upload

--- src/Component/Home/Login/firebase.config.js
+++ src/Component/Home/Login/firebase.config.js
@@ -1,10 +1,10 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDPD8FMogPA-_TS-rq5R3OhFJZ3P2AEXzs",
-    authDomain: "gedgetfix-aa3e8.firebaseapp.com",
-    projectId: "gedgetfix-aa3e8",
-    storageBucket: "gedgetfix-aa3e8.appspot.com",
-    messagingSenderId: "868551777583",
-    appId: "1:868551777583:web:0180e3a2df8d2f3742d5da"
+ var firebaseConfig = {
+    apiKey: "AIzaSyCQFMgBcCGNwbStaCKB8XrelLK0ZKVCOPI",
+    authDomain: "gadget-fix-23e64.firebaseapp.com",
+    projectId: "gadget-fix-23e64",
+    storageBucket: "gadget-fix-23e64.appspot.com",
+    messagingSenderId: "988974696726",
+    appId: "1:988974696726:web:f2c8145da8d1507b546837"
   };
-
-export default firebaseConfig;
+  // Initialize Firebase
+export default  firebaseConfig;
